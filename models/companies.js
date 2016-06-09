6047811b03ca5c2f0e87b3236f324a077f23c0b5
Jan Ove Øyen <Jan Ove yen>
2016-06-09 18:52:26 +0200

    Added and tested company.phone and company.email validation

--- models/companies.js
+++ models/companies.js
@@ -3,7 +3,6 @@ function Companies() { }
 var validator = require('validator');
 
 Companies.prototype.addCompany = function(company) {
-
   //validate input
   if (
        !company
@@ -12,10 +11,20 @@ Companies.prototype.addCompany = function(company) {
     || company.orgNumber.toString().length != 9
     || !company.name
     || !(typeof company.name === 'string')
+    || ( //if phone exists then it must be int
+        (typeof company.phone !== 'undefined' && company.phone !== null)
+        && (!validator.isInt(company.phone + ""))
+       )
+    || (  //if email exists then it must be valid address
+        (typeof company.email !== 'undefined' && company.email !== null)
+        && (!validator.isEmail(company.email + ""))
+       )
   )
   {
     return false;
   }
+
+  return true;
 
 };
 
--- tests/models/companies-test.js
+++ tests/models/companies-test.js
@@ -5,62 +5,110 @@ var Companies = require('./../../models/companies')
 var companies
 var testCompany
 
+var resetTests = function() {
+  companies = new Companies();
+
+  testCompany = {
+    orgNumber: 999888777,
+    name: "Testfirma AS",
+    phone: 99887766/*,
+    email: "post@testfirma.as",
+    mailingAddress: "Portveien 2, 0123 Oslo",
+    contact: {
+      name: "Ola Nordmann",
+      email: "ola.nordmann@testfirma.as",
+      phone: 44332211
+    }*/
+  }
+}
 
 describe('Companies', function() {
 
-
   beforeEach(function() {
-        companies = new Companies();
-
-        testCompany = {
-          orgNumber: 999888777,
-          name: "Testfirma AS",
-          phone: 99887766,
-          email: "post@testfirma.as",
-          mailingAddress: "Portveien 2, 0123 Oslo",
-          contact: {
-            name: "Ola Nordmann",
-            email: "ola.nordmann@testfirma.as",
-            phone: 44332211
-          }
-        }
+        resetTests();
   })
 
 /*--------------------------------------------------------------------------*/
 /*------------------------ addCompany tests --------------------------------*/
 /*--------------------------------------------------------------------------*/
+  describe("addCompany()", function() {
 
-//test validation
+    describe("Validation", function() {
 
-  it('addCompany() should return false on no company', function() {
-    expect(companies.addCompany()).to.equal(false)
-    expect(companies.addCompany("")).to.equal(false)
-    expect(companies.addCompany(undefined)).to.equal(false)
-  })
+      it('should return false if no company', function() {
+        expect(companies.addCompany()).to.equal(false)
+        expect(companies.addCompany("")).to.equal(false)
+        expect(companies.addCompany(undefined)).to.equal(false)
+      })
 
-  it('addCompany() should return false if bad company.orgNumber', function() {
-    testCompany.orgNumber = null
-    expect(companies.addCompany(testCompany)).to.equal(false)
+      it('should return false if bad company.orgNumber', function() {
+        testCompany.orgNumber = null
+        expect(companies.addCompany(testCompany)).to.equal(false)
 
-    testCompany.orgNumber = "bad orgnr"
-    expect(companies.addCompany(testCompany)).to.equal(false)
+        testCompany.orgNumber = "bad orgnr"
+        expect(companies.addCompany(testCompany)).to.equal(false)
 
-    testCompany.orgNumber = 1234
-    expect(companies.addCompany(testCompany)).to.equal(false)
+        testCompany.orgNumber = 1234
+        expect(companies.addCompany(testCompany)).to.equal(false)
 
-    testCompany.orgNumber = 1.3456789
-    expect(companies.addCompany(testCompany)).to.equal(false)
-  })
+        testCompany.orgNumber = 1.3456789
+        expect(companies.addCompany(testCompany)).to.equal(false)
 
-  it('addCompany() should return false if bad company.name', function() {
-    testCompany.name = null
-    expect(companies.addCompany(testCompany)).to.equal(false)
+        testCompany.orgNumber = 999888777
+        expect(companies.addCompany(testCompany)).to.equal(true)
 
-    testCompany.name = 1234
-    expect(companies.addCompany(testCompany)).to.equal(false)
+      })
 
-    testCompany.name = ""
-    expect(companies.addCompany(testCompany)).to.equal(false)
+      it('should return false if bad company.name', function() {
+        testCompany.name = null
+        expect(companies.addCompany(testCompany)).to.equal(false)
+
+        testCompany.name = 1234
+        expect(companies.addCompany(testCompany)).to.equal(false)
+
+        testCompany.name = ""
+        expect(companies.addCompany(testCompany)).to.equal(false)
+
+        testCompany.name = "Testfirma AS"
+        expect(companies.addCompany(testCompany)).to.equal(true)
+      })
+
+      it('should return false if bad company.phone exists', function() {
+
+        testCompany.phone = null
+        expect(companies.addCompany(testCompany)).to.equal(true)
+
+        testCompany.phone = undefined
+        expect(companies.addCompany(testCompany)).to.equal(true)
+
+        testCompany.phone = ""
+        expect(companies.addCompany(testCompany)).to.equal(false)
+
+        testCompany.phone = "bad phone number"
+        expect(companies.addCompany(testCompany)).to.equal(false)
+
+        testCompany.phone = "99887766"
+        expect(companies.addCompany(testCompany)).to.equal(true)
+      })
+
+      it('should return false if bad company.email exists', function() {
+
+            testCompany.email = undefined
+            expect(companies.addCompany(testCompany)).to.equal(true)
+
+            testCompany.email = null
+            expect(companies.addCompany(testCompany)).to.equal(true)
+
+            testCompany.email = ""
+            expect(companies.addCompany(testCompany)).to.equal(false)
+
+            testCompany.email = "bad email"
+            expect(companies.addCompany(testCompany)).to.equal(false)
+
+      })
+
+    })
+
   })
 
 })
