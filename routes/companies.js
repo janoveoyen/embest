c3f0b306f39308303cd64373813f4fb19e79614a
Jan Ove Øyen <janoveoyen@gmail.com>
2016-06-27 19:25:00 +0200

    Implemented companies/add with valid orgNumber

--- routes/companies.js
+++ routes/companies.js
@@ -1,3 +1,7 @@
 module.exports = function(req, res, next){
+  if(req.body.orgNumber != undefined) {
+    return res.status(200).end('');
+  }
+  
   return res.status(422).end('');
 }
--- tests/routes/companies-test.js
+++ tests/routes/companies-test.js
@@ -1,7 +1,12 @@
 var supertest = require('supertest');
- var app = require('../../app');
+var app = require('../../app');
+var Chai = require('chai');
+var expect = Chai.expect;
+
+
 
 describe("Companies routes",function(){
+
 
   it("/companies/add without parameters should return 422",function(done){
 
@@ -9,6 +14,21 @@ describe("Companies routes",function(){
       .post("/companies/add")
       .expect(422)
       .end(done);
+
+  });
+
+
+  it("/companies/add with valid orgNumber should return 200",function(done){
+
+    supertest(app)
+      .post("/companies/add")
+      .send({"orgNumber": "999888777"})
+      .expect(200)
+      .end(function(err, response) {
+          expect(err).to.equal(null);
+          console.log(response);
+          return done();
+      });
 
   });
 
