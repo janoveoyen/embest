43dcc85d9a808dda15f1636803e380da7a525cb4
Jan Ove Øyen <janoveoyen@gmail.com>
2016-10-24 00:31:15 +0200

    Add and test company-panel wrapper div, title and company-name input with label

--- components/company/Company.js
+++ components/company/Company.js
@@ -1,7 +1,15 @@
+const React = require('react');
+
 module.exports = {
 
-    Company: function(props) {
-        return null;
-    }
+  Company: function(props) {
+    return (
+      <div className="company-panel">
+        <h2 className='company-panel__title'>Bedriftsinformasjon</h2>
+        <label className='company-panel__label' for='company-name' />
+        <input className='company-panel__input'/>
+      </div>
+    );
+  }
 
 }
--- tests/components/company/Company-test.js
+++ tests/components/company/Company-test.js
@@ -4,13 +4,72 @@ const expect = require('chai').expect;
 const Enzyme = require('enzyme');
 const shallow = Enzyme.shallow;
 
-var Company = require('./../../../components/company/Company.js').Company;
+let Company = require('./../../../components/company/Company.js').Company;
+
+
 
 describe('Company component', function() {
 
-    it('should exist', function() {
-        var wrapper = shallow( < Company / > )
-        expect(wrapper).to.exist;
-    });
+
+
+  describe('with no  props', function() {
+
+    const wrapper = shallow(< Company / >)
+
+
+      it('should contain a wrapper div with class company-panel', function() {
+          const expectedClass = 'company-panel';
+          assertCorrectElement(wrapper, 'div', expectedClass, {} );
+      });
+
+
+      it('should contain a h2 with class="company-panel__title" ' +
+          ' and text "Bedriftsinformasjon"', function() {
+
+          const h2 = wrapper.childAt(0);
+          const expectedClass = 'company-panel__title';
+
+          assertCorrectElement(h2, 'h2', expectedClass, {} );
+
+          expect(h2.text()).to.equal('Bedriftsinformasjon');
+
+      });
+
+
+      it('should contain a label with class="company-panel__label" ' +
+          'and for="company-name"',  function() {
+
+        const label = wrapper.childAt(1);
+        const expectedClass = 'company-panel__label';
+        const expectedProp = {'for': 'company-name'}
+
+        assertCorrectElement( label, 'label', expectedClass, expectedProp);
+
+      });
+
+
+      it('should contain an input with class company-panel__input ' +
+          'and type="text" and name="company-name"', function() {
+
+        const input = wrapper.childAt(2);
+        const expectedClass = 'company-panel__input';
+
+        assertCorrectElement( input, 'input', expectedClass, {} );
+
+      });
+
+  });
+
+  function assertCorrectElement(el, expectType, expectClass, expectProps) {
+    const missingClassMsg = 'missing class ' + expectClass;
+
+    expect(el.type()).to.equal(expectType);
+    expect(el.hasClass(expectClass)).to.equal(true, missingClassMsg);
+
+    for (let prop in expectProps) {
+      expect(el.prop(prop)).to.equal(expectProps[prop]);
+    }
+
+  }
 
 });
