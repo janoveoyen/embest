16d94ed49f584d49435f937bbc55d670e10444c2
Jan Ove Øyen <janoveoyen@gmail.com>
2017-07-14 00:54:06 +0200

    Added the rest of the form fields for adding new company

--- components/company/Company.js
+++ components/company/Company.js
@@ -4,10 +4,42 @@ module.exports = {
 
   Company: function(props) {
     return (
-      <div className="company-panel">
-        <h2 className='company-panel__title'>Bedriftsinformasjon</h2>
-        <label className='company-panel__label' for='company-name' />
-        <input className='company-panel__input' type='text' name='company-name' />
+      <div className='panel'>
+        <h2 className='panel__title'>Bedriftsinformasjon</h2>
+        <form className='form'>
+          <div className='form__field'>
+            <label className='form__label' for='company-orgnr'>Org.nr</label>
+            <input className='form__input' type='text'  name='company-orgnr'
+              id='company-orgnr' />
+          </div>
+          <div className='form__field'>
+            <label className='form__label' for='company-name'>Bedriftsnavn</label>
+            <input className='form__input' type='text' name='company-name'
+              id='company-name'/>
+          </div>
+          <div className='form__field'>
+            <label className='form__label' for='company-salesperson'>Selger</label>
+            <select className='form__select' name='company-salesperson'
+              id='company-salesperson'></select>
+          </div>
+          <div className='form__field'>
+            <label className='form__label' for='company-phone'>Telefon</label>
+            <input className='form__input' type='tel' name='company-phone'
+              id='company-phone'/>
+          </div>
+          <div className='form__field'>
+            <label className='form__label' for='company-email'>E-post</label>
+            <input className='form__input' type='email' name='company-email'
+              id='company-email'></input>
+          </div>
+          <div className='form__field'>
+            <label className='form__label' for='company-mailingaddress'>
+              Fakturaadresse
+            </label>
+            <textarea className='form__textarea' name='company-mailingaddress'
+              id='company-mailingaddress'></textarea>
+          </div>
+        </form>
       </div>
     );
   }
--- tests/components/company/Company-test.js
+++ tests/components/company/Company-test.js
@@ -10,65 +10,305 @@ let Company = require('./../../../components/company/Company.js').Company;
 
 describe('Company component', function() {
 
+  let wrapper;
+  let form;
 
+  describe('with no props', function() {
+    wrapper = shallow(< Company / >)
+    form = wrapper.childAt(1);
+console.log(wrapper.debug())
 
-  describe('with no  props', function() {
+      it('should contain a <div class="panel" />', function() {
 
-    const wrapper = shallow(< Company / >)
-
-
-      it('should contain a wrapper div with class company-panel', function() {
-          const expectedClass = 'company-panel';
-          assertCorrectElement(wrapper, 'div', expectedClass, {} );
+        const expectedWrapper = {
+          'type' : 'div',
+          'class' : 'panel'
+        }
+        assertCorrectElement(wrapper, expectedWrapper);
       });
 
 
-      it('should contain a h2 with class="company-panel__title" ' +
-          ' and text "Bedriftsinformasjon"', function() {
+      it('should contain a <h2 class="panel__title">Bedriftsinformasjon</h2>',
+        function() {
 
-          const h2 = wrapper.childAt(0);
-          const expectedClass = 'company-panel__title';
+        const h2 = wrapper.childAt(0);
+        const expectedH2 = {
+          'type' : 'h2',
+          'class' : 'panel__title',
+          'text' : 'Bedriftsinformasjon'
+        }
 
-          assertCorrectElement(h2, 'h2', expectedClass, {} );
-
-          expect(h2.text()).to.equal('Bedriftsinformasjon');
-
-      });
-
-
-      it('should contain a label with class="company-panel__label" ' +
-          'and for="company-name"',  function() {
-
-        const label = wrapper.childAt(1);
-        const expectedClass = 'company-panel__label';
-        const expectedProp = {'for': 'company-name'}
-
-        assertCorrectElement( label, 'label', expectedClass, expectedProp);
+        assertCorrectElement(h2, expectedH2);
 
       });
 
 
-      it('should contain an input with class company-panel__input ' +
-          'and type="text" and name="company-name"', function() {
+      it('should contain a <form class="form" />', function() {
 
-        const input = wrapper.childAt(2);
-        const expectedClass = 'company-panel__input';
-        const expectedProps = {'type': 'text', 'name': 'company-name'}
+        const expectedForm = {
+          'type' : 'form',
+          'class' : 'form'
+        }
 
-        assertCorrectElement( input, 'input', expectedClass, expectedProps);
+        assertCorrectElement( form, expectedForm);
+
+      });
+
+      it('should contain a ' +
+        '<div class="form__field">' +
+        '<label class="form__label" for="company-orgnr">Org.nr</label>' +
+        '<input class="form__input" type="text" name="company-orgnr"' +
+        ' id="company-orgnr" />' +
+        '</div>', function() {
+
+        const fieldPosition = 0;
+
+        const expectedLabel = {
+          'type' : 'label',
+          'class':'form__label',
+          'text':'Org.nr',
+          'expectedProps' : {
+            'for' : 'company-orgnr'
+          }
+        }
+
+        const expectedControl = {
+          'type' : 'input',
+          'class': 'form__input',
+          'expectedProps' : {
+            'type': 'text',
+            'name': 'company-orgnr',
+            'id': 'company-orgnr'
+          }
+        }
+
+        const expectedField = {
+          'class' : 'form__field',
+          'label' : expectedLabel,
+          'control' : expectedControl
+        }
+
+        assertCorrectField(fieldPosition, expectedField);
+      });
+
+      it('should contain a ' +
+        '<div class="form__field">' +
+        '<label class="form__label" for="company-name">Bedriftsnavn</label>' +
+        '<input class="form__input" type="text" name="company-name"' +
+        ' id="company-name" />' +
+        '</div>', function() {
+
+        const fieldPosition = 1;
+
+        const expectedLabel = {
+          'type' : 'label',
+          'class':'form__label',
+          'text':'Bedriftsnavn',
+          'expectedProps' : {
+            'for' : 'company-name'
+          }
+        }
+
+        const expectedControl = {
+          'class':'form__input',
+          'type' : 'input',
+          'expectedProps' : {
+            'type' : 'text',
+            'name' : 'company-name',
+            'id' : 'company-name'
+          }
+        }
+
+        const expectedField = {
+          'class' : 'form__field',
+          'label' : expectedLabel,
+          'control' : expectedControl
+        }
+
+        assertCorrectField(fieldPosition, expectedField);
+
+      });
+
+      it('should contain a ' +
+        '<div class="form__field">' +
+        '<label class="form__label" for="company-salesperson">Selger</label>' +
+        '<select class="form__select" name="company-salesperson"'+
+        ' id="company-salesperson" />' +
+        '</div>', function() {
+
+        const fieldPosition = 2;
+
+        const expectedLabel = {
+          'type' : 'label',
+          'class':'form__label',
+          'text':'Selger',
+          'expectedProps' : {
+            'for' : 'company-salesperson'
+          }
+        }
+
+        const expectedControl = {
+          'class':'form__select',
+          'type' : 'select',
+          'expectedProps' : {
+            'name' : 'company-salesperson',
+            'id' : 'company-salesperson'
+          }
+        }
+
+        const expectedField = {
+          'class' : 'form__field',
+          'label' : expectedLabel,
+          'control' : expectedControl
+        }
+
+        assertCorrectField(fieldPosition, expectedField);
+
+      });
+
+    it('should contain a ' +
+      '<div class="form__field">' +
+      '<label class="form__label" for="company-phone">Telefon</label>' +
+      '<input class="form__input" type="tel" name="company-phone"' +
+      ' id="company-phone" />' +
+      '</div>', function() {
+
+      const fieldPosition = 3;
+
+      const expectedLabel = {
+        'type' : 'label',
+        'class':'form__label',
+        'text':'Telefon',
+        'expectedProps' : {
+          'for' : 'company-phone'
+        }
+      }
+
+      const expectedControl = {
+        'class':'form__input',
+        'type' : 'input',
+        'expectedProps' : {
+          'type' : 'tel',
+          'name' : 'company-phone',
+          'id' : 'company-phone'
+        }
+      }
+
+      const expectedField = {
+        'class' : 'form__field',
+        'label' : expectedLabel,
+        'control' : expectedControl
+      }
+
+      assertCorrectField(fieldPosition, expectedField);
+
+    });
+
+    it('should contain a ' +
+      '<div class="form__field">' +
+      '<label class="form__label" for="company-email">E-post</label>' +
+      '<input class="form__input" type="email" name="company-email"' +
+      ' id="company-email" />' +
+      '</div>', function() {
+
+      const fieldPosition = 4;
+
+      const expectedLabel = {
+        'type' : 'label',
+        'class':'form__label',
+        'text':'E-post',
+        'expectedProps' : {
+          'for' : 'company-email'
+        }
+      }
+
+      const expectedControl = {
+        'class':'form__input',
+        'type' : 'input',
+        'expectedProps' : {
+          'type' : 'email',
+          'name' : 'company-email',
+          'id' : 'company-email'
+        }
+      }
+
+      const expectedField = {
+        'class' : 'form__field',
+        'label' : expectedLabel,
+        'control' : expectedControl
+      }
+
+      assertCorrectField(fieldPosition, expectedField);
+
+    });
+
+      it('should contain a ' +
+        '<div class="form__field">' +
+        '<label class="form__label" for="company-mailingaddress">' +
+        'Fakturaadresse</label>' +
+        '<textarea class="form__textarea" name="company-mailingaddress"' +
+        ' id="company-mailingaddress" />' +
+        '</div>', function() {
+
+        const fieldPosition = 5;
+
+        const expectedLabel = {
+          'type' : 'label',
+          'class':'form__label',
+          'text':'Fakturaadresse',
+          'expectedProps' : {
+            'for' : 'company-mailingaddress'
+          }
+        }
+
+        const expectedControl = {
+          'class':'form__textarea',
+          'type' : 'textarea',
+          'expectedProps' : {
+            'name' : 'company-mailingaddress',
+            'id' : 'company-mailingaddress'
+          }
+        }
+
+        const expectedField = {
+          'class' : 'form__field',
+          'label' : expectedLabel,
+          'control' : expectedControl
+        }
+
+        assertCorrectField(fieldPosition, expectedField);
 
       });
 
   });
 
-  function assertCorrectElement(el, expectType, expectClass, expectProps) {
-    const missingClassMsg = 'missing class ' + expectClass;
 
-    expect(el.type()).to.equal(expectType);
-    expect(el.hasClass(expectClass)).to.equal(true, missingClassMsg);
+  function assertCorrectField(fieldPosition, expectedField) {
 
-    for (let prop in expectProps) {
-      expect(el.prop(prop)).to.equal(expectProps[prop]);
+      const field = form.childAt(fieldPosition);
+      const expectedClass = expectedField.class;
+      const label = field.childAt(0);
+      const control = field.childAt(1);
+
+      const missingClassMsg = "Missing class " + expectedClass;
+      expect(field.hasClass(expectedClass)).to.equal(true, missingClassMsg);
+
+      assertCorrectElement(label, expectedField.label);
+      assertCorrectElement(control, expectedField.control);
+  }
+
+  function assertCorrectElement(el, expectedElement) {
+    const expectedClass = expectedElement.class;
+    const missingClassMsg = "Missing class " + expectedClass;
+    expect(el.type()).to.equal(expectedElement.type);
+    expect(el.hasClass(expectedClass)).to.equal(true, missingClassMsg);
+
+    if (expectedElement.text) {
+      expect(el.text()).to.equal(expectedElement.text);
+    }
+
+    for (let prop in expectedElement.expectedProps) {
+      expect(el.prop(prop)).to.equal(expectedElement.expectedProps[prop]);
     }
 
   }
